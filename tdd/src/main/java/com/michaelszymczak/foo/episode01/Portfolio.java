7e22da11fc5b28b0b1916acfbf157a162087ecd3
Michael Szymczak <contact@michaelszymczak.com>
2017-02-07 21:22:05 +0000

    Refactoring of Portfolio - step 02

--- tdd/src/main/java/com/michaelszymczak/foo/episode01/Portfolio.java
+++ tdd/src/main/java/com/michaelszymczak/foo/episode01/Portfolio.java
@@ -2,22 +2,22 @@ package com.michaelszymczak.foo.episode01;
 
 public class Portfolio {
 
-  private final boolean hasFunds;
+  private final Funds funds;
 
   public static Portfolio emptyWithoutFunds()
   {
-    return new Portfolio(false);
+    return new Portfolio(Funds.ofValue(0));
   }
 
-  private Portfolio(boolean hasFunds) {
-    this.hasFunds = hasFunds;
+  private Portfolio(Funds funds) {
+    this.funds = funds;
   }
 
   public boolean hasFunds() {
-    return hasFunds;
+    return funds.available();
   }
 
   public Portfolio afterAdding(Funds fundsToAdd) {
-    return new Portfolio(true);
+    return new Portfolio(fundsToAdd);
   }
 }
