7e6caf96668d2a50fa4fd9b4c6f0f61d555f8e61
Michael Szymczak <contact@michaelszymczak.com>
2017-02-09 22:41:34 +0000

    Red Green on CompanyShares - preparation for Portfolio refactoring

--- tdd/src/main/java/com/michaelszymczak/foo/episode01/CompanyShares.java
+++ tdd/src/main/java/com/michaelszymczak/foo/episode01/CompanyShares.java
@@ -17,4 +17,8 @@ public class CompanyShares extends Value
   public int getQuantity() {
     return quantity;
   }
+
+  public int worthOn(StockMarket stockMarket) {
+    return stockMarket.priceOf(company).value() * quantity;
+  }
 }
---
+++ tdd/src/test/groovy/com/michaelszymczak/foo/episode01/CompanySharesShould.groovy
@@ -0,0 +1,18 @@
+package com.michaelszymczak.foo.episode01
+
+import spock.lang.Specification
+
+class CompanySharesShould extends Specification {
+  def "calculate how much they are worth based on stock market"() {
+    given:
+    def stockMarket = StockMarket.trading([
+            Share.identifiedBy(Share.ticker("FOO")).andPricePerShare(10),
+            Share.identifiedBy(Share.ticker("BAR")).andPricePerShare(20),
+            Share.identifiedBy(Share.ticker("BAZ")).andPricePerShare(30)
+    ] as Set)
+
+    expect:
+    new CompanyShares(Share.ticker("FOO"), 5).worthOn(stockMarket) == 50
+    new CompanyShares(Share.ticker("BAZ"), 100).worthOn(stockMarket) == 3000
+  }
+}
