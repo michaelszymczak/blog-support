511d3ff6af5be79c2076e0dd5673cc3cb00ba994
Michael Szymczak <contact@michaelszymczak.com>
2017-02-11 20:28:32 +0000

    Coati : Red - create first test case. Green - create empty methods in the test

--- coati-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Coati.java
+++ coati-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Coati.java
@@ -2,16 +2,16 @@ package com.michaelszymczak.blogsupport.coati;
 
 public class Coati {
 
-    public static void main(String[] args) {
-        System.out.println("                                      ");
-        System.out.println("                              .::     ");
-        System.out.println("                              .::     ");
-        System.out.println("    .:::   .::       .::    .:.: .:  /");
-        System.out.println("  .::    .::  .::  .::  .::   .::   //");
-        System.out.println(" .::    .::    .::.::   .::   .::  // ");
-        System.out.println("  .::    .::  .:: .::   .::   .::     ");
-        System.out.println("    .:::   .::      .:: .:::   .::()  ");
-        System.out.println("                                      ");
-        Coati coati = new Coati();
-    }
+  public static void main(String[] args) {
+    System.out.println("                                      ");
+    System.out.println("                              .::     ");
+    System.out.println("                              .::     ");
+    System.out.println("    .:::   .::       .::    .:.: .:  /");
+    System.out.println("  .::    .::  .::  .::  .::   .::   //");
+    System.out.println(" .::    .::    .::.::   .::   .::  // ");
+    System.out.println("  .::    .::  .:: .::   .::   .::     ");
+    System.out.println("    .:::   .::      .:: .:::   .::()  ");
+    System.out.println("                                      ");
+    new Coati();
+  }
 }
--- coati-tdd/src/test/groovy/com/michaelszymczak/blogsupport/coati/CoatiShouldInvestInTheStockMarketTest.groovy
+++ coati-tdd/src/test/groovy/com/michaelszymczak/blogsupport/coati/CoatiShouldInvestInTheStockMarketTest.groovy
@@ -2,40 +2,38 @@ package com.michaelszymczak.blogsupport.coati
 
 import spock.lang.Specification
 
-class CoatiShouldInvestInTheStockMarketTest extends Specification
-{
+class CoatiShouldInvestInTheStockMarketTest extends Specification {
+
   def "buy some shares having enough funds"() {
     given:
-    'stock market listing' 'GOOG sold USD 813.67 per share'
-    'available funds' 'USD 100000'
-    'no shares'
+    'stock market listing GOOG sold USD 813.67 per share'()
+    'available funds USD 100000'()
+    'no shares'()
 
     when:
-    'bought' '10 of GOOG'
+    'bought 10 of GOOG'()
 
     then:
-    'available funds should be' 'USD 91863.30'
-    'should have' '10 shares of GOOG'
+    'available funds should be USD 91863.30'()
+    'should have 10 shares of GOOG'()
   }
 
-  private static void "available funds"(String s) {
-    
+  private void 'stock market listing GOOG sold USD 813.67 per share'() {
   }
 
-  private static void "stock market listing"(String s) {
-
+  private void 'available funds USD 100000'() {
   }
 
-  private static void "bought"(String s) {
-
+  private void 'no shares'() {
   }
 
-
-  private static void "should have"(String s) {
-    assert true
+  private void 'bought 10 of GOOG'() {
   }
 
-  private static void "available funds should be"(String s) {
-    assert true
+  private void 'available funds should be USD 91863.30'() {
   }
+
+  private void 'should have 10 shares of GOOG'() {
+  }
+
 }
