3b2aeb692c3156950af6f880519fab05103eaeb2
Michael Szymczak <contact@michaelszymczak.com>
2017-02-12 23:27:24 +0000

    Coati Assets Refactor: Delegate grouping to a separate inner class

--- coati-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Assets.java
+++ coati-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Assets.java
@@ -3,8 +3,6 @@ package com.michaelszymczak.blogsupport.coati;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -22,16 +20,7 @@ public class Assets extends Value {
   }
 
   private Assets(List<CompanyShares> shares) {
-    this.shares = grouped(copyOf(checkNotNull(shares)));
-  }
-
-  private List<CompanyShares> grouped(List<CompanyShares> companyShares) {
-
-    return companyShares.stream()
-            .collect(Collectors.groupingBy(CompanyShares::ticker, Collectors.toList()))
-            .entrySet().stream()
-            .map(company -> CompanyShares.of(company.getKey(), company.getValue().stream().mapToLong(CompanyShares::howMany).sum()))
-            .collect(Collectors.toList());
+    this.shares = copyOf(new Grouped(shares).shares());
   }
 
   public Assets withAdditional(List<CompanyShares> shares) {
@@ -41,5 +30,22 @@ public class Assets extends Value {
             .build();
 
     return Assets.of(sharesAfterTransaction);
+  }
+
+  private static class Grouped extends Value {
+    private final List<CompanyShares> shares;
+
+    public Grouped(List<CompanyShares> shares) {
+      this.shares = checkNotNull(shares);
+    }
+
+    List<CompanyShares> shares()
+    {
+      return shares.stream()
+              .collect(Collectors.groupingBy(CompanyShares::ticker, Collectors.toList()))
+              .entrySet().stream()
+              .map(company -> CompanyShares.of(company.getKey(), company.getValue().stream().mapToLong(CompanyShares::howMany).sum()))
+              .collect(Collectors.toList());
+    }
   }
 }
