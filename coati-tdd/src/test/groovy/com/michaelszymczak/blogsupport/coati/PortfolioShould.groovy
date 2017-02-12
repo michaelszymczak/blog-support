d5be6fda71d2f9b85afbbcb7cc7c333309698d0d
Michael Szymczak <contact@michaelszymczak.com>
2017-02-12 00:52:31 +0000

    Coati Portfolio Test Refactor: parametrise to show the need of further development

--- coati-tdd/src/test/groovy/com/michaelszymczak/blogsupport/coati/PortfolioShould.groovy
+++ coati-tdd/src/test/groovy/com/michaelszymczak/blogsupport/coati/PortfolioShould.groovy
@@ -8,17 +8,27 @@ class PortfolioShould extends Specification {
   def "deduct the price of the bought share from available funds"() {
     given:
     Portfolio portfolio = Portfolio
-            .tradingOn(StockMarket.listing([new ListedCompany("FOO", Money.parse("USD 10"))]))
-            .with(Money.parse("USD 60"), noShares())
+            .tradingOn(StockMarket.listing([new ListedCompany(ticker, pricePerShare)]))
+            .with(initialFunds, noShares())
 
     when:
-    def portfolioAfterBuying = portfolio.afterBuying([new CompanyShares("FOO", 1)])
+    def portfolioAfterBuying = portfolio.afterBuying([new CompanyShares(ticker, 1)])
+
 
     then:
-    portfolio.funds() == Money.parse("USD 60")
+    portfolio.funds() == initialFunds
     portfolio.shares() == noShares()
-    portfolioAfterBuying.funds() == Money.parse("USD 50")
-    portfolioAfterBuying.shares() == [new CompanyShares("FOO", 1)]
+    portfolioAfterBuying.funds() == expectedFundsAfterThePurchase
+    portfolioAfterBuying.shares() == [new CompanyShares(ticker, 1)]
+
+    where:
+    ticker | pricePerShare | initialFunds | expectedFundsAfterThePurchase
+    "FOO"  | usd(10)       | usd(60)      | usd(50)
+//    "BAR"  | usd(1)        | usd(100)     | usd(99) // TODO
+  }
+
+  private static Money usd(def howMany) {
+    Money.parse("USD $howMany")
   }
 
   private static List<CompanyShares> noShares() {
