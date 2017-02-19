7d21ee60f3b6eac163e8867f7e59c21c52e5ae99
Michael Szymczak <contact@michaelszymczak.com>
2017-02-19 22:37:01 +0000

    Coati2: If my portfolio could buy shares... 2

--- coati2-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Coati.java
+++ coati2-tdd/src/main/java/com/michaelszymczak/blogsupport/coati/Coati.java
@@ -6,7 +6,7 @@ import static com.google.common.collect.ImmutableList.of;
 
 public class Coati {
 
-  private Portfolio onlyUserPortfolio = Portfolio.with(Assets.with(of(CompanyShares.of(new Ticker("GOOG"), 10))), Money.parse("USD 91863.30"));
+  private Portfolio onlyUserPortfolio = null;
 
   public static void main(String[] args) {
     System.out.println("                                      ");
@@ -20,13 +20,12 @@ public class Coati {
     System.out.println("                                      ");
   }
 
-  public Portfolio portfolioOf(User user) {
+  public synchronized Portfolio portfolioOf(User user) {
     return onlyUserPortfolio;
   }
 
   public synchronized void buyFor(User user, Assets newAssets) {
-
-
+    onlyUserPortfolio = Portfolio.with(Assets.with(of(CompanyShares.of(new Ticker("GOOG"), 10))), Money.parse("USD 91863.30"));
   }
 
   public synchronized void addPortfolioFor(User user, Portfolio portfolio) {
