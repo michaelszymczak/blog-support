8df0bc226c04bd34b968cff4c37676670ab0b929
Michael Szymczak <contact@michaelszymczak.com>
2017-04-09 20:00:00 +0100

    [tdd-diamond] Diamond of B

--- tdd-diamond/src/main/java/com/michaelszymczak/diamond/Diamond.java
+++ tdd-diamond/src/main/java/com/michaelszymczak/diamond/Diamond.java
@@ -1,11 +1,18 @@
 package com.michaelszymczak.diamond;
 
 public class Diamond {
+
+  private char letter;
+
   public static Diamond of(char letter) {
-    return new Diamond();
+    return new Diamond(letter);
+  }
+
+  private Diamond(char letter) {
+    this.letter = letter;
   }
 
   public String rendered() {
-    return "A";
+    return letter == 'A' ? "A" : " A \nB B\n A ";
   }
 }
--- tdd-diamond/src/test/groovy/com/michaelszymczak/diamond/DiamondAcceptanceTest.groovy
+++ tdd-diamond/src/test/groovy/com/michaelszymczak/diamond/DiamondAcceptanceTest.groovy
@@ -8,4 +8,12 @@ class DiamondAcceptanceTest extends Specification {
     expect:
     Diamond.of('A' as char).rendered() == "A"
   }
+
+  def "creates diamond shape if more than one letter"() {
+    expect:
+    Diamond.of('B' as char).rendered() == " A " + "\n" +
+                                          "B B" + "\n" +
+                                          " A "
+
+  }
 }
